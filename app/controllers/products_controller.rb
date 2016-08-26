9e5a2efde10e567a4b765934b0f8fd2630b63c14
Introwind <asim1204@gmail.com>
2016-08-26 14:07:53 +0100

    Dynamic forms. Form_tag used for NAV search

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,7 +4,26 @@ class ProductsController < ApplicationController
   # GET /products
   # GET /products.json
   def index
-    @products = Product.all
+  if Rails.env.production?
+
+    if params[:q]
+      search_term = params[:q]
+      @products = Product.where("name ilike ?", "%#{search_term}%")
+    else
+      @products = Product.all
+    end
+
+  else
+
+    if params[:q]
+      search_term = params[:q]
+      @products = Product.where("name LIKE ?", "%#{search_term}%")
+    else
+      @products = Product.all
+    end
+
+  end
+
   end
 
   # GET /products/1
