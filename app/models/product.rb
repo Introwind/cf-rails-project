61884f4728c0004609e01d7ffc0dd9215d87e0cb
Introwind <asim1204@gmail.com>
2016-09-02 20:03:51 +0100

    5.7 comment model with rails g model, relationship with 'references' cmd. General styles added

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,15 @@
 class Product < ApplicationRecord
 	has_many :orders
+	has_many :comments
+
+	def highest_rated_comment
+		comments.rating_desc.first
+	end
+
+	def lowest_rated_comment
+		comments.rating_desc.last
+	end
+
+		scope :value, -> { where(price: 0..500) }
+
 end
