5ce8031a6613f791cb857f52967909abb78e9ef8
Introwind <asim1204@gmail.com>
2016-09-12 19:50:48 +0100

    use Rspec to test Product model: Average comments. Invalid new product - with empty name

--- spec/mailers/previews/user_mailer_preview.rb
+++ spec/mailers/previews/user_mailer_preview.rb
@@ -1,7 +1,7 @@
 # Preview all emails at http://localhost:3000/rails/mailers/user_mailer
 class UserMailerPreview < ActionMailer::Preview
 	def contact_form
-		UserMailer.contact_form("test@example.com", "Bigman", "Hello world. Just testing here!")
+		UserMailer.contact_form("test@example.com", "Bigman", "Hello hahasworld. Just ahahastesting gsaaha!")
 		
 	end
 end
