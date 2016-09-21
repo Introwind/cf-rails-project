4d70ff2896ab980ae2d9abf6fae926a2f8ab2912
Introwind <asim1204@gmail.com>
2016-09-21 19:05:14 +0100

    6.13 RealTime updates with ActionCable/web sockets. Broadcast @ comments w/new comment posted

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,4 +92,7 @@ Rails.application.configure do
                      :socket_failure_delay => 0.2,
                      :down_retry_delay => 60
                     }
+  config.web_socket_server_url = "wss://bikestask.herokuapp.com/cable"
+  config.action_cable.allowed_request_origins = ['https://bikestask.herokuapp.com', 'http://bikestask.herokuapp.com']
+
 end
