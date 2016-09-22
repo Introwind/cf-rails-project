4a55b8437f76993cd109c7588d6a335dcf002416
Introwind <asim1204@gmail.com>
2016-09-22 19:03:06 +0100

    6.14: ActiveJob(dev-env-only) and Advanced ActionCable

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,12 +6,12 @@ class CommentsController < ApplicationController
     	@comment = @product.comments.new(comment_params)
     	@comment.user = current_user
     	@comment.save
+    	@user = current_user
     	respond_to do |format|
     		if @comment.save
-    			ActionCable.server.broadcast 'product_channel', comment: @comment
     			format.html { redirect_to @product, notice: 'Review was created successfully.'}
     			format.json { render :show, status: :created, location: @product }
-    			format.js
+    			format.js    			
     		else
     			format.html { redirect_to @product, alert: 'Review was not saved successfully' }
     			format.json { render json: @comment.errors, status: :unprocessable_entity }
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -5,6 +5,7 @@ class Comment < ApplicationRecord
   	validates :user, presence: true #makes sure a user is logged in before
   	validates :product, presence: true #makes sure a product is valid
   	validates :rating, numericality: { only_integer: true } #makes sure a rating has been given
+  after_create_commit { CommentUpdateJob.perform_later(self, @user) }
 
   scope :rating_desc, -> { order(rating: :desc) }
   scope :rating_asc, -> { order(:rating) }
